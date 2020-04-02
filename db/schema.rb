139eeaae8bf264672b33bb31d40fddf29152b529
yuuka-lav <60505747+yuuka-lav@users.noreply.github.com>
2020-04-02 22:05:29 +0900

    Add error message
    エラーメッセージ
    サクセスメッセージの表示

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 2020_04_02_010859) do
 
   create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
+    t.string "title", null: false
+    t.text "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
